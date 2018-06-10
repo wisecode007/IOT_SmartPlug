541257c485b7be020e4af45b35b2088d4d655d13
wisecode007 <y.weiss@outlook.com>
2018-06-10 18:00:28 +0300

    Update IOTsmartPlug.ino

--- IOTsmartPlug.ino
+++ IOTsmartPlug.ino
@@ -5,13 +5,13 @@
                             //ThingSpeak Defines
 #define CONNECTOR "rest"
 #define SERVER_ADDR "api.thingspeak.com"
-#define APIKEY_THINGSPEAK "****************" //Your API Key
+#define APIKEY_THINGSPEAK "****************"  //Your API Key 
 
                            //Pins Defines
 #define DHT11PIN 3   //temp sensor
 #define ldrPin  A1   //light sensor
 #define relayPin 8   //5V relay
-#define pirPin 12    //motion sensor 
+#define pirPin 13    //motion sensor 
 
 dht DHT;       //For the DHT11 sensor
 
@@ -37,7 +37,7 @@ void loop() {
   int Light = analogRead(ldrPin);
   int Motion = digitalRead(pirPin);
 
-  
+
   Ciao.println(Temp);
   Ciao.println(Humi);
   Ciao.println(Light);
@@ -46,17 +46,24 @@ void loop() {
                                   //Sending The Data to ThingSpeak
   String uri = "/update?api_key=";
     uri += APIKEY_THINGSPEAK;
+    
     uri += "&field1=";
     uri += String(Temp);
+    
     uri += "&field2=";
     uri += String(Humi);
-    uri += "&field3=";
-    uri += String(Light);
+    
+//    uri += "&field3=";
+//    uri += String(Light);
+
+    uri += "&field4=";
+    uri += String(Motion);
+                            
+                                //Ciao data is limited to 64 character
 
 
   Ciao.println("Send data on ThingSpeak Channel");
-
-  CiaoData data = Ciao.write(CONNECTOR, SERVER_ADDR, uri);
+  CiaoData data = Ciao.write(CONNECTOR, SERVER_ADDR, uri); 
 
   if (!data.isEmpty()){
   Ciao.println( "State: " + String (data.get(1)) );
@@ -69,15 +76,18 @@ void loop() {
 
 
                            //Power Automation Controller
-  if (Temp > 19)
-    if(Light > 1500)
-      if(Motion == 0)          //no movment
+  if (Temp > 25){               
+    if(Light > 800){            //still the light is on
+      if(Motion == 0){          //no movment
         digitalWrite(relayPin, LOW);    //disable the power socket
+      }
+    }
+  }
   else
     digitalWrite(relayPin, HIGH);
 
 
-  delay(30000); // Thinkspeak policy for sending data
+  delay(10000); // Thinkspeak policy for sending data
 
 }
 
@@ -87,11 +97,9 @@ void loop() {
  *  
  *  Conected thro the web interface to the HotSpot created by my phone. 
  *  
- *  
  *  Using ThingSpeak for the cloud.
  *  Public ThingSpeak Cloud View:
  *  https://thingspeak.com/channels/453554
  *  
  * 
  * */
- */
